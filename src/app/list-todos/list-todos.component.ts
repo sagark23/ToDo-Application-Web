54ca719f0ca2021f8aeea9fcd0d179f68854e27c
Sagar Khandelwal <sagarkhandelwal23@gmail.com>
2019-05-25 14:55:42 +0530

    Added bootstrat css

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id : Number,
+    public description : String,
+    public isDone : Boolean,
+    public targetDate : Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,10 +18,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
-  todo = {
-    id: 1,
-    description: 'Learn to dance'
-  }
+  todos = [
+    new Todo(1, 'Learn to dance', false, new Date()),
+    new Todo(2, 'Become an expert in Angular', false, new Date()),
+    new Todo(3, 'Visit India', false, new Date())
+  ]
+ 
   constructor() { }
 
   ngOnInit() {

d8957b14542326cf8c70373b70034430bfdb0502
Sagar Khandelwal <sagarkhandelwal23@gmail.com>
2019-06-03 23:33:26 +0530

    Authenticate user based on basic authentication service from backend server and remove hardcoded authentication service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+import { BasicAuthenticationService } from '../service/basic-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -11,22 +12,36 @@ export class LoginComponent implements OnInit {
 
   username = 'sa'
   password = ''
-  errorMessage ='Invalid credentials!'
-  validCredentials=true;
+  errorMessage = 'Invalid credentials!'
+  validCredentials = true;
 
   constructor(private router: Router,
-    private authenticationService: HardcodedAuthenticationService) { }
+    private authenticationService: HardcodedAuthenticationService,
+    private basicAuthenticationService: BasicAuthenticationService) { }
 
   ngOnInit() {
   }
 
-  handleLogin(){
-    if(this.authenticationService.authenticate(this.username, this.password)){
+  handleLogin() {
+    if (this.authenticationService.authenticate(this.username, this.password)) {
       this.router.navigate(['welcome', this.username])
-      this.validCredentials=true
-    }else{
-      this.validCredentials=false
+      this.validCredentials = true
+    } else {
+      this.validCredentials = false
     }
     console.log(this.username);
+  }
+
+  handleBasicAuthLogin() {
+    this.basicAuthenticationService.executeAuthenticationService(this.username, this.password).subscribe(
+      data => {
+        console.log(data);
+        this.router.navigate(['welcome', this.username])
+        this.validCredentials = true
+      },
+      error => {
+        this.validCredentials = false
+      }
+    );
   }
 }
---
+++ src/app/service/basic-authentication.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+
+export class AuthenticationBean {
+
+  constructor(private message: String) {
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  executeAuthenticationService(username, password) {
+    let authString = this.createBasicAuthenticationHeader(username, password);
+    let headers = new HttpHeaders({
+      Authorization: authString
+    })
+    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicAuth`, { headers }).pipe(
+      map(
+        data => {
+          sessionStorage.setItem("authenticatedUser", username);
+          sessionStorage.setItem("token", authString);
+          return data;
+        }
+      )
+    );
+  }
+
+  createBasicAuthenticationHeader(username, password) {
+    return "Basic " + window.btoa(username + ":" + password);
+  }
+
+  isUserLoggedIn(): boolean {
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
+  }
+
+  isAuthenticatedUser(): boolean {
+    let user = sessionStorage.getItem('token');
+    return !(user === null);
+  }
+
+  logout() {
+    sessionStorage.removeItem('authenticatedUser');
+    sessionStorage.removeItem('token');
+
+  }
+
+  getAuthenticatedUser(): String {
+    return sessionStorage.getItem('authenticatedUser');
+  }
+
+  getAuthenticatedToken(): String {
+    if (this.getAuthenticatedUser())
+      return sessionStorage.getItem('token');
+  }
+}
